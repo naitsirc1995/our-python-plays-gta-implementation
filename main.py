1aa612e8dcf6d0aa10d2f3e582aa7461e1d2772e
Cristian David Chavez Quintero <cristian.chavez@jikkosoft.com>
2022-04-30 10:42:24 -0500

    fixed resolution problem

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pickle import NONE
 import numpy as np
 from PIL import ImageGrab
 import cv2
@@ -7,31 +8,36 @@ from directkeys import PressKey, W, A, S, D
 import pyautogui
 
 
+def roi(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
 
 def process_img(image):
     original_image = image
     # convert to gray
     processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # edge detection
-    processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    processed_img =  cv2.Canny(processed_img, threshold1 = 50, threshold2=300)
+    # vertices = np.array([[10,500],[10,300], [300,200], [500,200], [800,300], [800,500]], np.int32)
+    # processed_img = roi(processed_img, [vertices])
     return processed_img
 
 def main():    
-
     last_time = time.time()
     pyautogui.click(100,150)     
     i = 0     
-    while True:        
-        PressKey(W)
-        screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+    while True:                
+        screen =  np.array(ImageGrab.grab(bbox=(0,60,800,600)))
         #print('Frame took {} seconds'.format(time.time()-last_time))        
         new_screen = process_img(screen)
-        cv2.imshow('window', new_screen)                
-        if i == 0:            
+        cv2.imshow('window', new_screen)
+        if i == 0:
             #pyautogui.hotkey('alt', 'tab') #alt + tab linux
             time.sleep(2)
             pyautogui.click((1500,150))
-            print("I was just clicked")
+            
         i+=1
         #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB) )
         if cv2.waitKey(25) & 0xFF == ord('q'):            
