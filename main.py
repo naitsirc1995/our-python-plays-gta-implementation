3bf6ec9d14f25e32da8d8545e324dc98bfa8285e
Cristian David Chavez Quintero <cristian.chavez@jikkosoft.com>
2022-05-14 10:55:19 -0500

    Tried to detect the area of interest

--- main.py
+++ main.py
@@ -10,32 +10,42 @@ import pyautogui
 
 def roi(img, vertices):
     mask = np.zeros_like(img)
-    cv2.fillPoly(mask, vertices, 255)
-    masked = cv2.bitwise_or(img, mask)
+    cv2.fillPoly(mask, vertices, 255)    
+    masked = cv2.bitwise_and(img, mask)
     return masked
 
 def process_img(image):
     original_image = image
     # convert to gray
     processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.threshold(processed_img,255,80,cv2.THRESH_BINARY)
     # edge detection
-    processed_img =  cv2.Canny(processed_img, threshold1 = 90, threshold2=300)
-    #vertices = np.array([[10,500],[10,300], [300,200], [500,200], [800,300], [800,500]], np.int32)
-    #processed_img = roi(processed_img, [vertices])
+    ##process_img = cv2.threshold(processed_img,)
+    # processed_img =  cv2.Canny(processed_img, threshold1 = 90, threshold2=300)
+    # vertices = np.array(
+    #     [            
+    #     [141,307],
+    #     [3,411], 
+    #     [785,397], 
+    #     [614,309]],
+    #     np.int32
+    # )
+    # processed_img = roi(processed_img, [vertices])
     return processed_img
 
 def main():    
     last_time = time.time()
     pyautogui.click(100,150)     
     i = 0     
-    while True:        
-        PressKey(W)
+    while True:                
         screen =  np.array(ImageGrab.grab(bbox=(0,60,800,600)))
         #print('Frame took {} seconds'.format(time.time()-last_time))
-        winname = "juego"
+        winname = "El juego de marciales"
         cv2.namedWindow(winname)
         cv2.moveWindow(winname,800,900)
         new_screen = process_img(screen)
+        new_screen = cv2.cvtColor(screen,cv2.COLOR_BGR2GRAY)
+        
         cv2.imshow(winname, new_screen)
         cv2.setWindowProperty("game",cv2.WND_PROP_TOPMOST,2)
         if i == 0:
